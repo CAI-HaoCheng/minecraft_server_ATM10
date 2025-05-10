a7b0b60648bb6100be168389ec5f77fd87a6f850
CAI-HaoCheng <qweewqqwe1236@gmail.com>
2025-05-10 22:58:20 +0800

    Update at 2025-05-10_22-58-01

--- ubuntu_git_push.sh
+++ ubuntu_git_push.sh
@@ -12,19 +12,44 @@ COMMIT_MESSAGE="Update at $CURRENT_DATE"
 # 讀取 GitHub 帳號和個人存取令牌
 . "/home/atmmc/github/.github_credentials"
 
-# 將檔案新增到 Git
-git add .
+# docker container名稱
+CONTAINER_NAME="ATM10"
 
-# 提交更改
-git commit -m "$COMMIT_MESSAGE"
+echo "正在停止 container: $CONTAINER_NAME"
+docker stop "$CONTAINER_NAME"
 
-echo  # 換行
+if [ $? -ne 0 ]; then
+    echo "停止 container 失敗，請確認名稱正確"
+    exit 1
+fi
 
-# 使用輸入的帳號和密碼進行身份驗證
-GIT_URL="https://$USERNAME:$TOKEN@github.com/CAI-HaoCheng/minecraft_server_ATM10.git"
+# 當伺服器關閉就執行備份
+while true; do
+    # 查詢目前contaier狀態
+    STATUS=$(docker inspect -f '{{.State.Status}}' "$CONTAINER_NAME" 2>/dev/null)
 
-# 推送到 GitHub
-git push "$GIT_URL" main
+    if [[ "$STATUS" == "exited" || "$STATUS" == "dead" ]]; then
+        echo "Container $CONTAINER_NAME 已關閉（狀態：$STATUS）"
+        # 將檔案新增到 Git
+        git add .
 
-# 完成提示
-echo "Files uploaded to GitHub."
+        # 提交更改
+        git commit -m "$COMMIT_MESSAGE"
+
+        echo # 換行
+
+        # 使用輸入的帳號和密碼進行身份驗證
+        GIT_URL="https://$USERNAME:$TOKEN@github.com/CAI-HaoCheng/minecraft_server_ATM10.git"
+
+        # 推送到 GitHub
+        git push "$GIT_URL" main
+
+        # 完成提示
+        echo "Files uploaded to GitHub."
+        break
+    else
+        echo "Container $CONTAINER_NAME 尚未關閉（目前狀態：$STATUS）"
+        echo "正在執行關閉ATM10"
+    fi
+    sleep 5 # 每 5 秒檢查一次
+done
